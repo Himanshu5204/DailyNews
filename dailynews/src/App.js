8fdfe2c5eeab004989063917ce9f88e2d3b4b741
Himanshu5204 <himanshuprajapati4695@gmail.com>
2025-06-12 19:48:29 +0530

    Loading logic done

--- dailynews/src/App.js
+++ dailynews/src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
       <div>
         {/* <p>{this.c}</p> */}
         <Navbar title="Daily News" />
-        <News/>
+        <News pageSize={5}/>
       </div>
     )
   }
--- dailynews/src/Components/News.js
+++ dailynews/src/Components/News.js
@@ -1,6 +1,7 @@
 // rce rcc react class component export statement
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor(props) {
@@ -28,8 +29,8 @@ export class News extends Component {
   // async func wait to resolve some promise in their body
   async componentDidMount() {
     console.log("News Component componentDidMount called");
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=54c1aec9ac8d4d60bebd523957581ce6&page=1&pageSize=20"; // url to fetch the news articles
+    let url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=54c1aec9ac8d4d60bebd523957581ce6&page=1&pageSize=${this.props.pageSize}`; // url to fetch the news articles
+    this.setState({ loading: true }); // set loading to true before fetching the data
     let data = await fetch(url); // fetch take url and return a promise
     // resolve or return
     // we use async await to handle the promise
@@ -50,11 +51,13 @@ export class News extends Component {
     console.log("Previous");
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=54c1aec9ac8d4d60bebd523957581ce6&page=${
       this.state.page - 1
-    }&pageSize=20`; // url to fetch the news articles
+    }&pageSize=${this.props.pageSize}`; // url to fetch the news articles
+    this.setState({ loading: true }); // set loading to true before fetching the data
     let data = await fetch(url); // fetch the data from the url
     let parsedData = await data.json(); // convert the data to json format
     console.log("Data", parsedData); // see the data in console
     this.setState({
+      loading: false, // set loading to false after fetching the data
       page: this.state.page - 1, // set the page state to the previous page
       articles: parsedData.articles, // set the articles state to the articles from the parsed data
     });
@@ -64,10 +67,14 @@ export class News extends Component {
     console.log("Next");
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=54c1aec9ac8d4d60bebd523957581ce6&page=${
       this.state.page + 1
-    }&pageSize=20`; // url to fetch the news articles   
+    }&pageSize=${this.props.pageSize}`; // url to fetch the news articles    
+
+    this.setState({ loading: true }); // set loading to true before fetching the data
+    
     let data = await fetch(url); // fetch the data from the url
     let parsedData = await data.json(); // convert the data to json format
     console.log("Data", parsedData); // see the data in console
+    this.setState({ loading: false }); // set loading to false after fetching the data
     this.setState({
       page: this.state.page + 1, // set the page state to the next page
       articles: parsedData.articles, // set the articles state to the articles from the parsed data
@@ -81,12 +88,12 @@ export class News extends Component {
         {/* {this.state.articles.map((element)=>{console.log(element)})} 
         articles is an array of objects load in console
         */}
-
+        {this.state.loading && <Spinner />}
         <div className="row">
           {!this.state.articles ? (
             <p className="text-center">No news articles found.</p>
           ) : (
-            this.state.articles.map((element) => (
+            !this.state.loading && this.state.articles.map((element) => (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
                   title={element.title ? element.title : " "}
@@ -96,15 +103,14 @@ export class News extends Component {
                   newsUrl={element.url}
                 />
               </div>
-            ))
-          )}
+            )))}
         </div>
         <div className="container d-flex justify-content-between ">
           <button type="button" className="btn btn-dark"  disabled={this.state.page <= 1} onClick={this.handlePrevClick}  >
             &larr; Previous 
             {/* onClick={() => this.setState({ page: this.state.page - 1 })} */}
           </button>
-          <button type="button" className="btn btn-dark" disabled={this.state.page >= Math.ceil(this.state.totalResults / 20)} onClick={this.handleNextClick} > 
+          <button type="button" className="btn btn-dark" disabled={this.state.page >= Math.ceil(this.state.totalResults / this.props.pageSize)} onClick={this.handleNextClick} > 
             Next 	&rarr;
             {/* onClick={() => this.setState({ page: this.state.page + 1 })}
                   */}
