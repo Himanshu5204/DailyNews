9200927ec12606e4e5ef8d44bee9cb5190d7f04d
Himanshu5204 <himanshuprajapati4695@gmail.com>
2025-06-12 18:42:16 +0530

    Previous Next arrow add now logic start

--- dailynews/src/Components/News.js
+++ dailynews/src/Components/News.js
@@ -3,64 +3,6 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  articles = [
-    {
-      source: { id: "news24", name: "News24" },
-      author: "Sibusiso Mjikeliso",
-      title:
-        "Cricket SA wants to 'get to the bottom' of Nkwe resignation concerns, says CEO | Sport",
-      description:
-        "Acting Cricket South Africa CEO Pholetsi Moseki says the board is concerned about the issues former Proteas assistant coach Enoch Nkwe raised in his resignation.",
-      url: "https://www.news24.com/sport/Cricket/Proteas/cricket-sa-wants-to-get-to-the-bottom-of-nkwe-resignation-concerns-says-ceo-20210826",
-      urlToImage:
-        "https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg",
-      publishedAt: "2021-08-26T11:40:16+00:00",
-      content:
-        '<ul><li>Cricket South Africa has committed to "getting to the bottom" of Enoch Nkwe\'s sudden resignation as Proteas assistant coach. </li><li>Nkwe voiced concerns with the current culture and working… [+3497 chars]',
-    },
-    {
-      source: { id: "abc-news-au", name: "ABC News (AU)" },
-      author: "ABC News",
-      title: "England cricket great Ted Dexter dies aged 86",
-      description:
-        'The former England captain, nicknamed "Lord Ted", is fondly remembered as a giant of the game and one of his country\'s greatest players.',
-      url: "http://www.abc.net.au/news/2021-08-26/england-cricket-great-ted-dexter-dies-aged-86/100411276",
-      urlToImage:
-        "https://live-production.wcms.abc-cdn.net.au/1006c7ecf34ec0935eef2aade5017711?impolicy=wcms_crop_resize&cropH=2815&cropW=5004&xPos=0&yPos=223&width=862&height=485",
-      publishedAt: "2021-08-26T09:07:52Z",
-      content:
-        "Former England captain Ted Dexter has died aged 86 following a recent illness.\r\n<ul><li>Dexter played 62 Tests for England and was captain on 30 occasions</li><li>He was inducted into the ICC Hall of… [+1746 chars]",
-    },
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
-
   constructor(props) {
     super(props); // Call the parent constructor must be called
     // You can initialize state here if needed
@@ -72,8 +14,9 @@ export class News extends Component {
     //props can not be changed read only
     //props passed and then state can be changed
     this.state = {
-      articles: this.articles,
+      articles: [], // articles is an null array of objects
       loading: false,
+      page: 1, // page is 1
     };
   }
 
@@ -83,20 +26,21 @@ export class News extends Component {
   // async func wait to resolve some promise in their body
   async componentDidMount() {
     console.log("News Component componentDidMount called");
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=54c1aec9ac8d4d60bebd523957581ce6";
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=54c1aec9ac8d4d60bebd523957581ce6&page=1&pageSize=20"; // url to fetch the news articles
     let data = await fetch(url); // fetch take url and return a promise
-                                // resolve or return
-                                // we use async await to handle the promise
-                                // fetch is a promise based function
+    // resolve or return
+    // we use async await to handle the promise
+    // fetch is a promise based function
     console.log(data);
     let parsedData = await data.json(); // convert the data to json format
-    console.log("Data", parsedData); //see the data in console 
-                                   // parsedData is an object with articles and totalResults properties
-                                 // articles is an array of objects with properties like source, author, title, description, url, urlToImage, publishedAt, content
+    console.log("Data", parsedData); //see the data in console
+    // parsedData is an object with articles and totalResults properties
+    // articles is an array of objects with properties like source, author, title, description, url, urlToImage, publishedAt, content
     this.setState({
       articles: parsedData.articles, // set the articles state to the articles from the parsed data
-                                     //totalResults: parsedData.totalResults, // set the total results state to the total results from the parsed data
-                                     //loading: false, // set loading to false
+      //totalResults: parsedData.totalResults, // set the total results state to the total results from the parsed data
+      //loading: false, // set loading to false
     });
   }
 
@@ -109,14 +53,14 @@ export class News extends Component {
         */}
 
         <div className="row">
-          {! this.state.articles ? (
+          {!this.state.articles ? (
             <p className="text-center">No news articles found.</p>
           ) : (
             this.state.articles.map((element) => (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
-                  title={element.title?element.title.slice(0, 45):" "}
-                  description={element.description?element.description.slice(0, 88):""}
+                  title={element.title ? element.title : " "}
+                  description={element.description ? element.description : ""}
                   imageUrl={element.urlToImage}
                   author={element.author || "Unknown"}
                   newsUrl={element.url}
@@ -124,6 +68,14 @@ export class News extends Component {
               </div>
             ))
           )}
+        </div>
+        <div className="container d-flex justify-content-between ">
+          <button type="button" class="btn btn-dark">
+            &larr; Previous
+          </button>
+          <button type="button" class="btn btn-dark">
+            Next 	&rarr;
+          </button>
         </div>
       </div>
     );
