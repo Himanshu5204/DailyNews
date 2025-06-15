b818eb53eb4f1f6863224835fbac0f3e45a0c2e4
Himanshu5204 <himanshuprajapati4695@gmail.com>
2025-06-15 10:58:25 +0530

    top loading bar added

--- dailynews/src/App.js
+++ dailynews/src/App.js
@@ -6,6 +6,7 @@ import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
 // This is a class component in React.
 // It extends the Component class from React and defines a render method.
@@ -14,6 +15,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export default class App extends Component {
   c = "This is a class component";
   pageSize = 5;
+  state = {
+    progress:0
+  }
+
+  setProgress = (progress)=>{
+    this.setState({progress: progress})
+  }
   render() {
     //render lifecycle methods
     return (
@@ -23,17 +31,22 @@ export default class App extends Component {
         <News pageSize={5} country="us" category="science"/>
         */}
         <Router>
-          
+
           <Navbar />
+          <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={this.state.progress} 
+      />
           <Switch>
-            <Route exact path="/"><News key="general" pageSize={this.pageSize} country="us" category="general"/></Route> 
-            <Route exact path="/business"><News key="business" pageSize={this.pageSize} country="us" category="business"/></Route>
-          <Route exact path="/entertainment"><News key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/></Route> 
-          <Route exact path="/general"><News key="general" pageSize={this.pageSize} country="us" category="general"/></Route> 
-          <Route exact path="/health"><News key="health" pageSize={this.pageSize} country="us" category="health"/></Route> 
-          <Route exact path="/science"><News key="science" pageSize={this.pageSize} country="us" category="science"/></Route> 
-          <Route exact path="/sports"><News key="sports" pageSize={this.pageSize} country="us" category="sports"/></Route> 
-          <Route exact path="/technology"><News key="technology" pageSize={this.pageSize} country="us" category="technology"/></Route>
+            <Route exact path="/"><News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="us" category="general"/></Route> 
+            <Route exact path="/business"><News setProgress={this.setProgress} key="business" pageSize={this.pageSize} country="us" category="business"/></Route>
+          <Route exact path="/entertainment"><News setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country="us" category="entertainment"/></Route> 
+          <Route exact path="/general"><News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country="us" category="general"/></Route> 
+          <Route exact path="/health"><News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country="us" category="health"/></Route> 
+          <Route exact path="/science"><News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country="us" category="science"/></Route> 
+          <Route exact path="/sports"><News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country="us" category="sports"/></Route> 
+          <Route exact path="/technology"><News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country="us" category="technology"/></Route>
           </Switch>
         </Router>
       </div>
--- dailynews/src/Components/News.js
+++ dailynews/src/Components/News.js
@@ -47,16 +47,21 @@ export class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=54c1aec9ac8d4d60bebd523957581ce6&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(70);
+    console.log("Data", parsedData);
 
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
   // componentDidMount is a lifecycle method that is called
   // after the constructor-->render-->componentDidmount is mounted
